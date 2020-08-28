6ab23bed99640b3d1a43ae1639243c9570dc5f38
Johnny Yip <jyippy2012@gmail.com>
2020-08-28 10:54:12 +0000

    alternate fizzbuzz + cleanup

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -6,8 +6,25 @@ def fizzbuzz(number)
     elsif number % 5 == 0
       return 'buzz'
     else 
-      return '.' 
+      return "#{number}" 
     end
 end
 
-p fizzbuzz(79)
+#p fizzbuzz(79)
+
+#alternate way of Fizzbuzz
+class Integer
+  def selfbuzz
+    if self % 3 == 0 && self % 5 == 0
+      return 'fizzbuzz'
+    elsif self % 3 == 0
+      return 'fizz'
+    elsif self % 5 == 0
+      return 'buzz'
+    else 
+      return "#{self}"
+    end  
+  end
+end
+
+#p 15.selfbuzz
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -13,8 +13,29 @@ describe 'fizzbuzz' do
         expect(fizzbuzz(15)).to eq 'fizzbuzz'
     end 
     
-    it 'returns "." when passed 79' do
-        expect(fizzbuzz(79)).to eq '.'
-    end    
+    it 'returns "79" when passed 79' do
+        expect(fizzbuzz(79)).to eq '79'
+    end   
+
     
+end
+
+
+describe 'fizzbuzz alternate' do
+  it 'returns "fizz" for the number 12' do
+    expect(12.selfbuzz).to eq 'fizz'
+  end 
+
+  it 'returns "buzz" for the number 12' do
+    expect(25.selfbuzz).to eq 'buzz'
+  end 
+
+  it 'returns "fizzbuzz" for the number 30' do
+    expect(30.selfbuzz).to eq 'fizzbuzz'
+  end 
+
+  it 'returns "44" for the number 44' do
+    expect(44.selfbuzz).to eq '44'
+  end 
+
 end
